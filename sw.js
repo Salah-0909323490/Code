837aa1626d2ce34bc3e127d60f2db6e80a48bb6a
Salah-0909323490 <salahq445@gmail.com>
2026-08-19 13:10:33 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'abu-alsolh-v1';
+const CACHE_NAME = 'abu-alsolh-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
